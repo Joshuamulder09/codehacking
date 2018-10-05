8f46def82b2457c5711bf13d50746961a283ae6c
Joshua <joshua.mulder@hotmail.com>
2018-10-05 15:28:31 +0200

    users section working 100% - except security

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -7,6 +7,7 @@
         <thead>
         <tr>
             <th>Id</th>
+            <th>Photo</th>
             <th>Name</th>
             <th>Email</th>
             <th>Role</th>
@@ -19,19 +20,23 @@
         @if($users)
 
             @foreach($users as $user)
-        <tr>
-            <td>{{$user->id}}</td>
-            <td>{{$user->name}}</td>
-            <td>{{$user->email}}</td>
-            <td>{{$user->role ? $user->role->name : 'User has no role'}}</td>//aantal uren gecreerd
-            <td>{{$user->is_active == 1 ? 'Active' : 'Not active' }}</td> als user is active 1 is doe dan active anders not active
-            <td>{{$user->created_at->diffForHumans()}}</td> //aantal uren gecreerd
-            <td>{{$user->updated_at->diffForHumans()}}</td>
-        </tr>
+                <tr>
+                    <td>{{$user->id}}</td>
+                    <td> <img height="50" src="{{$user->photo ? $user->photo->file : 'http://placehold.it/400x400'}}" alt="" ></td>
+                    <td><a href="{{route('admin.users.edit', $user->id)}}">{{$user->name}}</a></td>
+                    <td>{{$user->email}}</td>
+                    <td>{{$user->role ? $user->role->name : 'User has no role'}}</td>
+                    //aantal uren gecreerd
+                    <td>{{$user->is_active == 1 ? 'Active' : 'Not active' }}</td>
+                    als user is active 1 is doe dan active anders not active
+                    <td>{{$user->created_at->diffForHumans()}}</td>
+                    //aantal uren gecreerd
+                    <td>{{$user->updated_at->diffForHumans()}}</td>
+                </tr>
 
             @endforeach
 
-            @endif
+        @endif
 
         </tbody>
     </table>
